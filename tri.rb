ef5a5328e821935137d79c739076f261f65257c7
山崎慎悟 <yamazakishingo@yamazakishingonoMacBook-Pro.local>
2021-07-15 16:47:59 +0900

    ruby coding

--- tri.rb
+++ tri.rb
@@ -1,7 +1,7 @@
 # require 'matrix'
 # m = Matrix[[1,1,1],[1,0,0],[0,1,0]]
 # v = Vector[7,3,1]
-# p (m**47*v)[0]  #トリボナッチ数列の50項の値を求める
+# p (m**27*v)[0]  #トリボナッチ数列の50項の値を求める
 
 
 
@@ -21,3 +21,35 @@
 # ##フィボナッチ数列一般項1
 
 
+# a = 1
+# b = 0
+# c = 5
+# n = 0
+# while n < 27
+#   d = a + b + c
+#   a = b
+#   b = c
+#   c = d
+#   n += 1
+# end
+
+# puts c
+
+# i = 1
+# a = 0
+# while i <= 20000 do
+#   if i % 3 == 0 || i.to_s.include?("3")
+#     a += 1
+#   end
+#   i += 1
+# end
+# p a
+num = 1
+sum = 0
+  for num in 1..20 do
+    if num % 3 == 0 || num.to_s.include?("3")
+     sum += num.to_i
+    end
+    num = num + 1
+  end
+  p sum
